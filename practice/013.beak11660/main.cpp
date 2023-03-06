fe5567c22a1b9b6491f6c5df95ea1058b9d61375
LimNakHyun <dlaskrgus8@gmail.com>
2023-03-06 22:26:34 +0900

    백준 11660번 구간 합 구하기 5 [완료]

--- practice/013.beak11660/main.cpp
+++ practice/013.beak11660/main.cpp
@@ -10,6 +10,10 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     int n, m;
     cin >> n >> m;
     cout << "n: " << n << "\n";
@@ -21,13 +25,13 @@ int main()
         a[i] = new int[n + 1]();
     }
 
-    cout << "초기화한 배열" << endl;
-    for(int i = 0; i < n + 1; i++) {
-        for(int j = 0; j < n + 1; j++) {
-            cout << a[i][j] << " ";
-        }
-        cout << "\n";
-    }
+    // cout << "초기화한 배열" << endl;
+    // for(int i = 0; i < n + 1; i++) {
+    //     for(int j = 0; j < n + 1; j++) {
+    //         cout << a[i][j] << " ";
+    //     }
+    //     cout << "\n";
+    // }
 
     for(int i = 1; i < n + 1; i++) {
         for(int j = 1; j < n + 1; j++) {
@@ -37,12 +41,18 @@ int main()
         }
     }
 
-    cout << "값을 넣은 배열" << endl;
-    for(int i = 0; i < n + 1; i++) {
-        for(int j = 0; j < n + 1; j++) {
-            cout << a[i][j] << "   ";
-        }
-        cout << "\n\n";
+    // cout << "값을 넣은 배열" << endl;
+    // for(int i = 0; i < n + 1; i++) {
+    //     for(int j = 0; j < n + 1; j++) {
+    //         cout << a[i][j] << "   ";
+    //     }
+    //     cout << "\n\n";
+    // }
+
+    for(int i = 0; i < m; i++) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        cout << a[x2][y2] - (a[x1 - 1][y2] + a[x2][y1 - 1]) + a[x1 - 1][y1 - 1] << "\n"; 
     }
 
     for(int i = 0; i < n + 1; i++) {
