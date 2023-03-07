4fd0f1372cd601474036394ad380cdd1da677148
LimNakHyun <dlaskrgus8@gmail.com>
2023-03-07 15:42:18 +0900

    백준 10986번 나머지 합 [완료]

--- practice/014.beak10986/main.cpp
+++ practice/014.beak10986/main.cpp
@@ -10,19 +10,45 @@ using namespace std;
 
 int main()
 {
-    int n, m;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long n, m;
     cin >> n >> m;
 
-    int *a;
-    a = new int[n + 1]();
+    long *a, *b;
+    a = new long[n + 1]();
+    b = new long[n + 1]();
 
     for(int i = 1; i <= n; i ++) {
-        int temp;
+        long temp;
         cin >> temp;
-        a[i] = a[i - 1] + temp;
+        a[i] = (a[i - 1] + temp) % m;
+        b[a[i]]++;
     }
 
+    // for(int i = 0; i < n + 1; i++) {
+    //     cout << a[i] << " ";
+    // }
+    // cout << "\n";
+    // for(int i = 0; i < n + 1; i++) {
+    //     cout << b[i] << " ";
+    // }
+    // cout << "\n";
+
+    long ans = 0;
+    ans += b[0];
     for(int i = 0; i < n + 1; i++) {
-        cout << a[i] << " ";
+        if(b[i] != 0) {
+            ans += (b[i] * (b[i] - 1)) / 2;
+            // cout << "\n" << (b[i] * (b[i] - 1)) / 2;
+        }
     }
+
+    // cout << "\n";
+    cout << ans << endl;
+
+    delete [] a;
+    delete [] b;
 }
