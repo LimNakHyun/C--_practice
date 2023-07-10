03b9865e3b489ebaf9eacdde707009dc0f501df7
LimNakHyun <dlaskrgus8@gmail.com>
2023-07-10 13:19:26 +0900

    1-4. 연산자 (1)

---
+++ sparta/1-4/arithmeticoperators.cpp
@@ -0,0 +1,18 @@
+// arithmeticoperators.cpp
+#include <iostream>   // 이건 출력을 위한 헤더 가져오기
+
+int main()
+{
+    using namespace std;
+
+    int a = 10;
+    int b = 3;
+
+    cout << "a+b=" << a+b << endl;
+    cout << "a-b=" << a-b << endl;
+    cout << "a*b=" << a*b << endl;
+    cout << "a/b=" << a/b << endl;
+    cout << "a%b=" << a%b << endl;
+
+    return 0;
+}
---
+++ sparta/1-4/watermelons.cpp
@@ -0,0 +1,18 @@
+// watermelons.cpp
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    int numOfWatermelons = 3;
+    int bonus = 2;
+
+    cout << "I had " << numOfWatermelons << " watermelons" << endl;
+
+    numOfWatermelons = numOfWatermelons + bonus;
+
+    cout << "I have " << numOfWatermelons << " watermelons" << endl;
+
+    return 0;
+}
