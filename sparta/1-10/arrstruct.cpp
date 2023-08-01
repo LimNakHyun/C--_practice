aa0d9a88785b7b61750f983acea967075f8deba8
LimNakHyun <dlaskrgus8@gmail.com>
2023-08-01 12:47:22 +0900

    스파르타 C++ 1-10. 구조체 (완료)

---
+++ sparta/1-10/arrstruct.cpp
@@ -0,0 +1,23 @@
+// arrstruct.cpp
+#include <iostream>
+
+using namespace std;
+
+struct Person
+{
+    string name;
+    int age;
+};
+
+int main()
+{
+    Person students[2] = {
+        {"John Doe", 20},
+        {"Jane", 23}
+		};
+
+    cout << students[0].name << endl;
+    cout << students[1].name << endl;
+
+    return 0;
+}
